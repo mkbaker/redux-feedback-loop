0a3a08138a8b8883550db501a8befb8e60844df3
Kellen Baker <mkellenbaker@gmail.com>
2019-06-14 14:58:34 -0500

    create redux store

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore, combineReducers } from 'redux';
+import { logger } from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const feelingReducer = (state = '', action) => {
+    return state;
+}
+
+const storeInstance = createStore(
+    combineReducers({
+        feelingReducer,
+    }),
+    applyMiddleware(logger)
+)
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
