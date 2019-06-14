4d7ac5655613ae3bdc84d315846b65b52c04279e
Kellen Baker <mkellenbaker@gmail.com>
2019-06-14 14:49:57 -0500

    add ReviewYourFeedback component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 //components
 import HowAreYouFeeling from '../HowAreYouFeeling/HowAreYouFeeling';
+import ReviewYourFeedback from '../ReviewYourFeedback/ReviewYourFeedback';
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
           <h4><i>Don't forget it!</i></h4>
         </header>
         <HowAreYouFeeling />
+        <ReviewYourFeedback />
         <br/>
       </div>
     );  
---
+++ src/components/ReviewYourFeedback/ReviewYourFeedback.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class ReviewYourFeedback extends Component {
+    render(){
+        return (
+            <div>
+                <h2>Review Your Feedback</h2>
+                <p>Feeling: </p>
+                <p>Understanding: </p>
+                <p>Support: </p>
+                <p>Comments: </p>
+            </div>
+        )
+    }
+}
+
+export default ReviewYourFeedback;
