b5f87979b1bdc60772ed864b6e992ec617598826
Kellen Baker <mkellenbaker@gmail.com>
2019-06-14 15:51:03 -0500

    add state  functionality to HowAreYouFeeling

--- src/components/HowAreYouFeeling/HowAreYouFeeling.js
+++ src/components/HowAreYouFeeling/HowAreYouFeeling.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class HowAreYouFeeling extends Component {
+    state = {
+        feeling: '',
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            feeling: event.target.value
+        })
+    }
 
     handleNext = () => {
         this.props.dispatch({
             type: 'SET_FEELING',
-            payload: 5, //CHANGE THIS TO REFLECT UPDATED LOCAL STATE
+            payload: this.state.feeling, 
         })
         this.props.history.push('/understanding');
     }
@@ -16,7 +25,7 @@ class HowAreYouFeeling extends Component {
                 <h2>How are you feeling today?</h2>
 
                 <span className="inputAndNext">
-                <input type="number" placeholder="Feeling?"/>
+                <input type="number" placeholder="Feeling?" onChange={this.handleChange}/>
                 <button onClick={this.handleNext}>Next</button>
                 </span>
 
